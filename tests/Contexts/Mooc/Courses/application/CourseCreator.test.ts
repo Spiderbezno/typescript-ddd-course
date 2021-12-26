78d3f6905163bb3a6a7975329fb209a763a626c6
Jorge Gomes <jorge.gomes@andorratelecom.ad>
2021-12-26 18:54:44 +0100

    Corections in Datainjections

--- tests/Contexts/Mooc/Courses/application/CourseCreator.test.ts
+++ tests/Contexts/Mooc/Courses/application/CourseCreator.test.ts
@@ -17,7 +17,7 @@ describe('CourseCreator', () => {
     const name = 'some-name';
     const duration = 'some-duration';
 
-    const course = new Course({id, name, duration});
+    const course = new Course(id, name, duration);
 
     await creator.run(id, name, duration);
 
