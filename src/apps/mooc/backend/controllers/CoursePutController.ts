dc0bb230ee50a809cb76c51a1e0b792524468612
Jorge Gomes <jorge.gomes@andorratelecom.ad>
2021-12-27 08:11:51 +0100

    DTO CurseCreator

--- src/apps/mooc/backend/controllers/CoursePutController.ts
+++ src/apps/mooc/backend/controllers/CoursePutController.ts
@@ -3,14 +3,19 @@ import httpStatus from 'http-status';
 import { Controller } from './Controller';
 import { CourseCreator } from '../../../../Contexts/Mooc/Courses/application/CourseCreator';
 
+type CourseCreatorRequest = Request & {
+  body: {
+    id: string;
+    name: string;
+    duration: string;
+  };
+};
 export class CoursePutController implements Controller {
   constructor(private courseCreator: CourseCreator) {}
 
-  async run(req: Request, res: Response) {
+  async run(req: CourseCreatorRequest, res: Response) {
     const { id, name, duration } = req.body;
-
-    await this.courseCreator.run(id, name, duration);
-
+    await this.courseCreator.run({id, name, duration});
     res.status(httpStatus.CREATED).send();
   }
 }
