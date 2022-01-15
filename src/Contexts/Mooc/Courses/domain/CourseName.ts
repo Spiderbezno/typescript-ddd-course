8a1875ee23d07d01e736638582d47480d2013612
Jorge Gomes <jorge.gomes@andorratelecom.ad>
2022-01-15 20:17:17 +0100

    Add more value objects

---
+++ src/Contexts/Mooc/Courses/domain/CourseName.ts
@@ -0,0 +1,15 @@
+import { InvalidArgumentError } from '../../../Shared/domain/value-object/InvalidArgumentError';
+import { StringValueObject } from '../../../Shared/domain/value-object/StringValueObject';
+
+export class CourseName extends StringValueObject {
+  constructor(value: string) {
+    super(value);
+    this.ensureLengthIsLessThan30Characters(value);
+  }
+
+  private ensureLengthIsLessThan30Characters(value: string): void {
+    if (value.length > 30) {
+      throw new InvalidArgumentError(`The Course Name <${value}> has more than 30 characters`);
+    }
+  }
+}
--- tests/Contexts/Mooc/Courses/infrastructure/persistence/FileCourseRepository.test.ts
+++ tests/Contexts/Mooc/Courses/infrastructure/persistence/FileCourseRepository.test.ts
@@ -1,10 +1,12 @@
 import { Course } from '../../../../../../src/Contexts/Mooc/Courses/domain/Course';
+import { CourseDuration } from '../../../../../../src/Contexts/Mooc/Courses/domain/CourseDuration';
+import { CourseName } from '../../../../../../src/Contexts/Mooc/Courses/domain/CourseName';
 import { FileCourseRepository } from '../../../../../../src/Contexts/Mooc/Courses/infrastructure/persistence/FileCourseRepository';
 import { Uuid } from '../../../../../../src/Contexts/Shared/domain/value-object/Uuid';
 
 describe('FileCourseRepository', () => {
   it('should save course', async () => {
-    const expectedCourse = new Course({id: new Uuid('0766c8f0-f9d6-4b3f-b8e8-f9d6f9d6f9d6'), name: 'name', duration: 'duration'});
+    const expectedCourse = new Course({id: new Uuid('0766c8f0-f9d6-4b3f-b8e8-f9d6f9d6f9d6'), name: new CourseName('name'), duration: new CourseDuration('duration') });
     const repository = new FileCourseRepository();
 
     await repository.save(expectedCourse);
