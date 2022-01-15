d7361c548fb6f820c9303eb02e5931ee778bb774
Jorge Gomes <jorge.gomes@andorratelecom.ad>
2022-01-15 19:37:14 +0100

    Add uuid

--- src/Contexts/Mooc/Courses/infrastructure/persistence/FileCourseRepository.ts
+++ src/Contexts/Mooc/Courses/infrastructure/persistence/FileCourseRepository.ts
@@ -1,5 +1,6 @@
 import { deserialize, serialize } from 'bson';
 import fs from 'fs';
+import { Uuid } from '../../../../Shared/domain/value-object/Uuid';
 import { Course } from '../../domain/Course';
 import { CourseRepository } from '../../domain/CourseRepository';
 
@@ -10,13 +11,13 @@ export class FileCourseRepository implements CourseRepository {
     fs.promises.writeFile(this.filePath(course.id), serialize(course));
   }
 
-  async search(courseId: string): Promise<Course> {
+  async search(courseId: Uuid): Promise<Course> {
     const courseData = await fs.promises.readFile(this.filePath(courseId));
     const { id, name, duration } = deserialize(courseData);
-    return new Course(id, name, duration);
+    return new Course({id, name, duration});
   }
 
-  private filePath(id: string): string {
+  private filePath(id: Uuid): string {
     return `${this.FILE_PATH}.${id}.repo`;
   }
 }
--- tests/Contexts/Mooc/Courses/infrastructure/persistence/FileCourseRepository.test.ts
+++ tests/Contexts/Mooc/Courses/infrastructure/persistence/FileCourseRepository.test.ts
@@ -1,14 +1,15 @@
 import { Course } from '../../../../../../src/Contexts/Mooc/Courses/domain/Course';
 import { FileCourseRepository } from '../../../../../../src/Contexts/Mooc/Courses/infrastructure/persistence/FileCourseRepository';
+import { Uuid } from '../../../../../../src/Contexts/Shared/domain/value-object/Uuid';
 
 describe('FileCourseRepository', () => {
   it('should save course', async () => {
-    const expectedCourse = new Course('id', 'name', 'duration');
+    const expectedCourse = new Course({id: new Uuid('0766c8f0-f9d6-4b3f-b8e8-f9d6f9d6f9d6'), name: 'name', duration: 'duration'});
     const repository = new FileCourseRepository();
 
     await repository.save(expectedCourse);
 
-    const course = await repository.search('id');
+    const course = await repository.search(new Uuid('0766c8f0-f9d6-4b3f-b8e8-f9d6f9d6f9d6'));
     expect(course).toEqual(expectedCourse);
   });
 });
